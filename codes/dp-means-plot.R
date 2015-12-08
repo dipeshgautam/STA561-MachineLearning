e521d37221bb4146ac98bdbf14534885a17d0525
Dipesh Gautam <digtm123@gmail.com>
2015-12-08 01:39:04 -0500

    fixed dp-means-plot.R to read data properly

--- codes/dp-means-plot.R
+++ codes/dp-means-plot.R
@@ -1,6 +1,6 @@
 source("codes/dp-means.R")
 
-data1 = read.table("mcdonalds-normalized-data.txt", header = T, sep = "\t", comment.char = "", quote = "")
+data1 = read.table("data/mcdonalds-normalized-data.txt", header = T, sep = "\t", comment.char = "", quote = "")
 mcd <- data1[ ,1:14]
 colnames(mcd) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14')
 
