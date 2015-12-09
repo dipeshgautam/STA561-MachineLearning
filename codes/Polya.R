337559a28e77b533ed5ee35673152b61ebd60aa5
ra125 <ra125@duke.edu>
2015-12-08 22:10:40 -0500

    Update Polya.R

--- codes/Polya.R
+++ codes/Polya.R
@@ -4,12 +4,9 @@ polya_urn_model = function(base_color_distribution, num_balls, alpha) {
   
   for (i in 1:num_balls) {
     if (runif(1) < alpha / (alpha + length(balls))) {
-      # Add a new ball color.
       new_color = base_color_distribution()
       balls = c(balls, new_color)
     } else {
-      # Pick out a ball from the urn, and add back a
-      # ball of the same color.
       ball = balls[sample(1:length(balls), 1)]
       balls = c(balls, ball)
     }
